f16d9167ca768028977ef0a5650c42c7b672f41d
Abdullah Shublaq <abd.shublaq@gmail.com>
2019-10-25 10:29:39 +0300

    Clinic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin;
+use App\Appointment;
+use App\Patient;
+use App\Section;
+use App\UserGroup;
+
 class HomeController extends Controller
 {
     //
     public function index()
     {
-        return view('template.home');
+        $admins = Admin::count();
+        $usergroups = UserGroup::count();
+        $sections = Section::count();
+        $patients = Patient::count();
+        $appointments = Appointment::count();
+        $appointments_day = Appointment::where('visit_date', 'LIKE', '%' . date("Y-m-d") . '%')->count();
+
+        return view('template.home')->with('admins', $admins)->with('usergroups', $usergroups)->with('sections', $sections)
+            ->with('patients', $patients)->with('appointments', $appointments)->with('appointments_day', $appointments_day);
     }
 
 }
